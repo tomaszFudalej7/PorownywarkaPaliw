81310892836e1894b37c0b5fdc0ca95621c2b8d2
Kostiantyn Prysiazhnyi <kprysiazhnyi@gmail.com>
2017-04-21 16:26:17 +0300

    Add files via upload

---
+++ AdminChangeUserPermission.php
@@ -0,0 +1,32 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    require_once('AdminHelper.php');
+    $adminHelper = new AdminHelper();
+
+    $jsonResponse = array("error" => false);
+    if(isset($_POST['email']) && isset($_POST['permission'])){
+        $email = $_POST['email'];
+        $permissionToSet = $_POST['permission'];
+
+        $result = $adminHelper->updateUserPermissionStatus($permissionToSet,$email);
+        if($result == false){
+            $jsonResponse["error"] = true;
+            $jsonResponse["errorMessage"] = " Wrong email or permission please try again";
+            echo json_encode($jsonResponse);
+        }
+        else if($result == true){
+            $jsonResponse["error"] = false;
+            $jsonResponse["message"] = "Permission was successfully changed ;)";
+        }
+    }
+    else{
+        $jsonResponse["error"] = true;
+        $jsonResponse["errorMessage"] = "isset error";
+        echo json_encode($jsonResponse);
+    }
+}
+else{
+    $jsonResponse["error"] = true;
+    $jsonResponse["errorMessage"] = "method is not POST";
+    echo json_encode($jsonResponse);
+}
